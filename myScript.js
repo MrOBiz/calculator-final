6cde5e0d6b6efc477104a833dc5d3812fd6dfd25
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-10 08:34:25 +0200

    Added querySelector for buttons

--- myScript.js
+++ myScript.js
@@ -1,6 +1,26 @@
 let numA, numB;
 let operators = ["+", "-", "*", "/"];
 
+const display = document.querySelector("#display");
+
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const times = document.querySelector("#times");
+const divided = document.querySelector("#divided");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+
 
 function add(a, b){
     return a + b;
@@ -40,3 +60,4 @@ function operate(a, b, opr){
     if(opr == "/"){return divide(a, b)};
     if(opr == "*"){return multiply(a, b)}; */
 }
+
