6b0d3d103f5b863aebacf4cfeb1af945ae99851a
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-10 10:45:55 +0200

    Coded basic input handling inside listeners

--- myScript.js
+++ myScript.js
@@ -1,6 +1,8 @@
 let numA, numB;
 let operators = ["+", "-", "*", "/", "=", "c"];
-let flag;
+let flag = 0;
+let inputNumA = [];
+let inputNumB = [];
 let result;
 
 const display = document.querySelector("#display");
@@ -11,16 +13,39 @@ const numButtons = document.querySelectorAll(".num");
 
 operatorButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
-        return flag = operators[index];
+        flag = operators[index];
+        console.log(flag);
     });
 });
 
 numButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
-        
-    }),
+        if(flag == 0){
+            inputNumA.push(index);
+            numA = Number(inputNumA.join(""));
+            console.log("A " + numA);
+        }else{
+            inputNumB.push(index);
+            numB = Number(inputNumB.join(""));
+            console.log("B " + numB);
+        }
+    });
 });
 
+
+/* setOperands();
+
+function setOperands(){
+    if(flag == null){
+        numA = Number(inputNum.join(""));
+        console.log(numA);
+    }else if(flag != null){
+        console.log(flag);
+        numB = Number(inputNum.join(""));
+        console.log(numB);
+    }
+}
+ */
 function add(a, b){
     return a + b;
 }
